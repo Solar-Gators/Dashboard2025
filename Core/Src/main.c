8ebc817b81cdddee53e300c8c5b813abf7d370e1
samryan02 <samrbreslin@gmail.com>
2025-01-16 12:28:43 -0500

    comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -335,8 +335,16 @@ void StartTask02(void *argument)
 {
   /* USER CODE BEGIN StartTask02 */
   /* Infinite loop */
+
+  //create veraibe, for storing ADC variable
+
   for(;;)
   {
+	//Code for reading ADC values
+
+
+	//code sending data over CAN
+
     osDelay(1);
   }
   /* USER CODE END StartTask02 */
